90f6f93222d94a99d7116594051436386554ecd4
avrjakhov <a.v.rjakhov@yandex.ru>
2026-02-28 18:26:20 +0500

    fix: hide experience scroll hints at left and right boundaries

--- assets/js/home-nav.js
+++ assets/js/home-nav.js
@@ -50,4 +50,26 @@
 
   window.addEventListener('scroll', onScroll, { passive: true });
   onScroll();
+
+  var experienceWrap = document.querySelector('.experience-scroll-wrap');
+  var experienceGrid = experienceWrap ? experienceWrap.querySelector('.experience-grid') : null;
+
+  function updateExperienceHints() {
+    if (!experienceWrap || !experienceGrid) {
+      return;
+    }
+
+    var maxScrollLeft = Math.max(0, experienceGrid.scrollWidth - experienceGrid.clientWidth);
+    var scrollLeft = experienceGrid.scrollLeft;
+    var edgeTolerance = 2;
+
+    experienceWrap.classList.toggle('is-at-start', scrollLeft <= edgeTolerance);
+    experienceWrap.classList.toggle('is-at-end', scrollLeft >= (maxScrollLeft - edgeTolerance));
+  }
+
+  if (experienceWrap && experienceGrid) {
+    experienceGrid.addEventListener('scroll', updateExperienceHints, { passive: true });
+    window.addEventListener('resize', updateExperienceHints);
+    window.requestAnimationFrame(updateExperienceHints);
+  }
 })();
